d4e516d69930ea1ed84109d17ae8c19848aad284
NULL <74785274+wjx354@users.noreply.github.com>
2021-09-17 14:00:09 +0800

    Add files via upload

---
+++ usart/usart.h
@@ -0,0 +1,55 @@
+#ifndef __USART_H
+#define __USART_H	
+#include "sys.h" 
+
+//2,修改了USART_RX_STA,使得串口最大接收字节数为2的14次方
+//3,增加了USART_REC_LEN,用于定义串口最大允许接收的字节数(不大于2的14次方)
+//4,修改了EN_USART1_RX的使能方式
+
+#define USART_REC_LEN  			200  	//定义最大接收字节数 200
+#define EN_USART1_RX 			1		//使能（1）/禁止（0）串口1接收
+
+//if(USART_RX_STA&0x8000)
+//len=USART_RX_STA&0x3fff;得到此次接收到的数据长度
+	  	
+extern u8  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符 
+extern u16 USART_RX_STA;         		//接收状态标记	
+//如果想串口中断接收，请不要注释以下宏定义
+void uart_init(u32 bound);
+void usart1_send_char(u8 c);
+
+#endif
+
+/*
+
+while(1)
+	{
+		if(USART_RX_STA&0x8000)
+		{					   
+			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
+			printf("\r\n您发送的消息为:\r\n\r\n");
+			for(t=0;t<len;t++)
+			{
+				USART_SendData(USART1, USART_RX_BUF[t]);//向串口1发送数据
+				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
+			}
+			printf("\r\n\r\n");//插入换行
+			USART_RX_STA=0;
+		}else
+		{
+			times++;
+			if(times%5000==0)
+			{
+				printf("\r\n战舰STM32开发板 串口实验\r\n");
+				printf("正点原子@ALIENTEK\r\n\r\n");
+			}
+			if(times%200==0)printf("请输入数据,以回车键结束\n");  
+			if(times%30==0)LED0=!LED0;//闪烁LED,提示系统正在运行.
+			delay_ms(10);   
+		}
+	}	 
+ }
+
+*/
+
+
