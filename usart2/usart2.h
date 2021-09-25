e8b8a995610d747134b4030f3159296a2de45046
NULL <74785274+wjx354@users.noreply.github.com>
2021-09-25 10:22:23 +0800

    Add files via upload

---
+++ usart2/usart2.h
@@ -0,0 +1,16 @@
+#ifndef __USRAT2_H
+#define __USRAT2_H 
+
+#include "sys.h"	
+
+extern int32_t distance_left;
+extern int32_t degree_left;
+
+extern int32_t distance_right;
+extern int32_t degree_right;
+
+void uart2_init(u32 bound);
+
+
+#endif
+
